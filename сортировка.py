4156614bfc38e94f40c9919b6ebad15870414833
Artur <arturbaglikov2016@gmail.com>
2022-08-04 14:19:06 +0300

    n_new

--- сортировка.py
+++ сортировка.py
@@ -1,16 +1,28 @@
-name_f = input()
-f = open(name_f)
-text = f.read()
-text = text.split('\n')
-a = text[:-1]
-N = len(a)
-a = [int(x) for x in a]
+import asyncio
 
-for i in range(N - 1):
-    for j in range(N - i - 1):
-        if a[j] > a[j + 1]:
-            a[j], a[j + 1] = a[j + 1], a[j]
+import aiohttp
 
-print(a)
+name_f = input('Введите имя файла: ')
+url = 'https://raw.githubusercontent.com/Arthurs21/N_Normal1/main/samples/' + name_f
 
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.text(encoding='utf-8')
+    print(data)
+    d1 = data.split('\n')
+    N = len(d1)
+    a = [int(x) for x in d1]
+
+    for i in range(N - 1):
+        for j in range(N - i - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+
+    print(*a)
+
+
+asyncio.run(main())
+print()
 f = input()
