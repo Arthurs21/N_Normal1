4b9ba9815716bf53b811278d935780fc71ca855f
Artur <arturbaglikov2016@gmail.com>
2022-08-05 10:51:33 +0300

    sn

--- сортировка.py
+++ сортировка.py
@@ -10,19 +10,23 @@ async def main():
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.text(encoding='utf-8')
-    print(data)
-    d1 = data.split('\n')
-    N = len(d1)
-    a = [int(x) for x in d1]
+    if len(data) == 0:
+        print('Файл пустой')
+        f1 = input()
+    else:
+        d1 = data.split('\n')
+        N = len(d1)
+        a = [int(x) for x in d1]
 
-    for i in range(N - 1):
-        for j in range(N - i - 1):
-            if a[j] > a[j + 1]:
-                a[j], a[j + 1] = a[j + 1], a[j]
+        for i in range(N - 1):
+            for j in range(N - i - 1):
+                if a[j] > a[j + 1]:
+                    a[j], a[j + 1] = a[j + 1], a[j]
 
-    print(*a)
+        print(*a)
 
 
 asyncio.run(main())
 print()
 f = input()
+
